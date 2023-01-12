4caf6fc8cb45c9519ea802b24bea40bcababc260
Kalcio <mmunozm@utem.cl>
2023-01-12 00:00:53 -0300

    Created database connection

---
+++ lib/db_helper/database_connection.dart
@@ -0,0 +1,19 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseConnection {
+  Future<Database> setDatabase() async {
+    var directory = await getApplicationDocumentsDirectory();
+    var path = join(directory.path, 'db_crud');
+    var database =
+        await openDatabase(path, version: 1, onCreate: _createDatabase);
+    return database;
+  }
+
+  Future<void> _createDatabase(Database database, int version) async {
+    String sql =
+        "CREATE TABLE notes (id INTEGER PRIMARY KEY, name TEXT, autor TEXT, editorial TEXT, year TEXT, isbn TEXT)";
+    await database.execute(sql);
+  }
+}
