1c007dfa49f9695726cfc718d64e854d5bdbf755
Kalcio <mmunozm@utem.cl>
2023-01-25 03:06:52 -0300

    Created function add images

--- lib/pages/add_book.dart
+++ lib/pages/add_book.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:biblioapp/models/books_model.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:snippet_coder_utils/FormHelper.dart';
 
 class AddBook extends StatefulWidget {
@@ -15,7 +17,6 @@ class AddBook extends StatefulWidget {
 class _AddBookState extends State<AddBook> {
   GlobalKey<FormState> globalKey = GlobalKey<FormState>();
   late BookModel model;
-  List<dynamic> categories = [];
 
   @override
   void initState() {
@@ -40,148 +41,224 @@ class _AddBookState extends State<AddBook> {
 
   _formUI() {
     return SingleChildScrollView(
-        child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          FormHelper.inputFieldWidgetWithLabel(
-            context,
-            'name',
-            "Name Book",
-            "",
-            (onValidate) {
-              if (onValidate.isEmpty) {
-                return "* Required";
-              }
-            },
-            (onSaved) {
-              model.name = onSaved.toString().trim();
-            },
-            initialValue: model.name,
-            showPrefixIcon: true,
-            prefixIcon: const Icon(Icons.text_fields),
-            borderRadius: 10,
-            contentPadding: 15,
-            fontSize: 14,
-            labelFontSize: 14,
-            paddingLeft: 0,
-            paddingRight: 0,
-            prefixIconPaddingLeft: 10,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FormHelper.inputFieldWidgetWithLabel(
-            context,
-            'autor',
-            "Autor Book",
-            "",
-            (onValidate) {
-              if (onValidate.isEmpty) {
-                return "* Required";
-              }
-            },
-            (onSaved) {
-              model.autor = onSaved.toString().trim();
-            },
-            initialValue: model.name,
-            showPrefixIcon: true,
-            prefixIcon: const Icon(Icons.text_fields),
-            borderRadius: 10,
-            contentPadding: 15,
-            fontSize: 14,
-            labelFontSize: 14,
-            paddingLeft: 0,
-            paddingRight: 0,
-            prefixIconPaddingLeft: 10,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FormHelper.inputFieldWidgetWithLabel(
-            context,
-            'editorial',
-            "Editorial",
-            "",
-            (onValidate) {
-              if (onValidate.isEmpty) {
-                return "* Required";
-              }
-            },
-            (onSaved) {
-              model.editorial = onSaved.toString().trim();
-            },
-            initialValue: model.editorial ?? "",
-            showPrefixIcon: true,
-            prefixIcon: const Icon(Icons.text_fields),
-            borderRadius: 10,
-            contentPadding: 15,
-            fontSize: 14,
-            labelFontSize: 14,
-            paddingLeft: 0,
-            paddingRight: 0,
-            prefixIconPaddingLeft: 10,
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: FormHelper.inputFieldWidgetWithLabel(
-                  context,
-                  'year',
-                  "Year",
-                  "",
-                  (onValidate) {
-                    if (onValidate.isEmpty) {
-                      return "* Required";
-                    }
-                  },
-                  (onSaved) {
-                    model.year = int.parse(onSaved.trim());
-                  },
-                  initialValue: "",
-                  showPrefixIcon: true,
-                  prefixIcon: const Icon(Icons.numbers),
-                  borderRadius: 10,
-                  contentPadding: 15,
-                  fontSize: 14,
-                  labelFontSize: 14,
-                  paddingLeft: 0,
-                  paddingRight: 0,
-                  prefixIconPaddingLeft: 10,
-                  isNumeric: true,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            FormHelper.inputFieldWidgetWithLabel(
+              context,
+              'name',
+              "Name Book",
+              "",
+              (onValidate) {
+                if (onValidate.isEmpty) {
+                  return "* Required";
+                }
+              },
+              (onSaved) {
+                model.name = onSaved.toString().trim();
+              },
+              initialValue: model.name,
+              showPrefixIcon: true,
+              prefixIcon: const Icon(Icons.text_fields),
+              borderRadius: 10,
+              contentPadding: 15,
+              fontSize: 14,
+              labelFontSize: 14,
+              paddingLeft: 0,
+              paddingRight: 0,
+              prefixIconPaddingLeft: 10,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FormHelper.inputFieldWidgetWithLabel(
+              context,
+              'autor',
+              "Autor Book",
+              "",
+              (onValidate) {
+                if (onValidate.isEmpty) {
+                  return "* Required";
+                }
+              },
+              (onSaved) {
+                model.autor = onSaved.toString().trim();
+              },
+              initialValue: model.name,
+              showPrefixIcon: true,
+              prefixIcon: const Icon(Icons.text_fields),
+              borderRadius: 10,
+              contentPadding: 15,
+              fontSize: 14,
+              labelFontSize: 14,
+              paddingLeft: 0,
+              paddingRight: 0,
+              prefixIconPaddingLeft: 10,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FormHelper.inputFieldWidgetWithLabel(
+              context,
+              'editorial',
+              "Editorial",
+              "",
+              (onValidate) {
+                if (onValidate.isEmpty) {
+                  return "* Required";
+                }
+              },
+              (onSaved) {
+                model.editorial = onSaved.toString().trim();
+              },
+              initialValue: model.editorial ?? "",
+              showPrefixIcon: true,
+              prefixIcon: const Icon(Icons.text_fields),
+              borderRadius: 10,
+              contentPadding: 15,
+              fontSize: 14,
+              labelFontSize: 14,
+              paddingLeft: 0,
+              paddingRight: 0,
+              prefixIconPaddingLeft: 10,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: FormHelper.inputFieldWidgetWithLabel(
+                    context,
+                    'year',
+                    "Year",
+                    "",
+                    (onValidate) {
+                      if (onValidate.isEmpty) {
+                        return "* Required";
+                      }
+                    },
+                    (onSaved) {
+                      model.year = int.parse(onSaved.trim());
+                    },
+                    initialValue: "",
+                    showPrefixIcon: true,
+                    prefixIcon: const Icon(Icons.numbers),
+                    borderRadius: 10,
+                    contentPadding: 15,
+                    fontSize: 14,
+                    labelFontSize: 14,
+                    paddingLeft: 0,
+                    paddingRight: 0,
+                    prefixIconPaddingLeft: 10,
+                    isNumeric: true,
+                  ),
+                ),
+                Flexible(
+                  child: FormHelper.inputFieldWidgetWithLabel(
+                    context,
+                    'isbn',
+                    "ISBN",
+                    "",
+                    (onValidate) {
+                      if (onValidate.isEmpty) {
+                        return "* Required";
+                      }
+                    },
+                    (onSaved) {
+                      model.year = int.parse(onSaved.trim());
+                    },
+                    initialValue: "",
+                    showPrefixIcon: true,
+                    prefixIcon: const Icon(Icons.numbers),
+                    borderRadius: 10,
+                    contentPadding: 15,
+                    fontSize: 14,
+                    labelFontSize: 14,
+                    paddingLeft: 0,
+                    paddingRight: 0,
+                    prefixIconPaddingLeft: 10,
+                    isNumeric: true,
+                  ),
+                )
+              ],
+            ),
+            _picPicker(
+              model.image ?? "",
+              (file) => setState(
+                () {
+                  model.image = file.path;
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _picPicker(
+    String fileName,
+    Function onFilePicked,
+  ) {
+    Future<XFile?> _imageFile;
+    ImagePicker _picker = ImagePicker();
+
+    return Column(
+      children: [
+        fileName != ""
+            ? Image.file(
+                File(fileName),
+                width: 300,
+                height: 300,
+              )
+            : SizedBox(
+                width: 300,
+                height: 250,
+                child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Image.network(
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/255px-No-Image-Placeholder.svg.png"),
                 ),
               ),
-              Flexible(
-                child: FormHelper.inputFieldWidgetWithLabel(
-                  context,
-                  'isbn',
-                  "ISBN",
-                  "",
-                  (onValidate) {
-                    if (onValidate.isEmpty) {
-                      return "* Required";
-                    }
-                  },
-                  (onSaved) {
-                    model.year = int.parse(onSaved.trim());
-                  },
-                  initialValue: "",
-                  showPrefixIcon: true,
-                  prefixIcon: const Icon(Icons.numbers),
-                  borderRadius: 10,
-                  contentPadding: 15,
-                  fontSize: 14,
-                  labelFontSize: 14,
-                  paddingLeft: 0,
-                  paddingRight: 0,
-                  prefixIconPaddingLeft: 10,
-                  isNumeric: true,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 35,
+              width: 35,
+              child: IconButton(
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(
+                  Icons.image,
+                  size: 35,
                 ),
-              )
-            ],
-          )
-        ],
-      ),
-    ));
+                onPressed: () {
+                  _imageFile = _picker.pickImage(source: ImageSource.gallery);
+                  _imageFile.then((file) async {
+                    onFilePicked(file);
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: 35,
+              width: 35,
+              child: IconButton(
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(
+                  Icons.camera,
+                  size: 35,
+                ),
+                onPressed: () {
+                  _imageFile = _picker.pickImage(source: ImageSource.camera);
+                  _imageFile.then((file) async {
+                    onFilePicked(file);
+                  });
+                },
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
